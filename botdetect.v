7129de1730bcb3bf8bcb9244e1267805c3c5e406
Alexander Medvednikov <alexander@vlang.io>
2023-09-17 19:06:29 +0300

    bot urls

--- botdetect.v
+++ botdetect.v
@@ -1,11 +1,11 @@
 // Copyright (c) 2019-2023 Alexander Medvednikov. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
-module vweb
+module botdetect
 
 const bot_uas = [
 	'Googlebot',
-	'FreshPing',
+	'FreshpingBot',
 	'yacybot',
 	'Sogou web spider',
 	'Slackbot',
@@ -19,8 +19,27 @@ const bot_uas = [
 ]
 
 pub fn ua_is_bot(ua string) bool {
-	for bot in vweb.bot_uas {
+	for bot in botdetect.bot_uas {
 		if ua.contains(bot) {
+			return true
+		}
+	}
+	return false
+}
+
+const bot_urls = [
+	'/admin.php',
+	'/login.php?s=Admin/login',
+	'/wp-content/plugins/',
+	'/wp-admin/',
+	'/wp-includes/',
+	'/wp_filemanager.php',
+	'/wp-includes/wp-class.php',
+]
+
+pub fn url_is_requested_by_bot(url string) bool {
+	for bot_url in botdetect.bot_urls {
+		if url.starts_with(bot_url) {
 			return true
 		}
 	}
